e71f8808dfb36a6963cae7b871bbd1bacf617fe0
Ko-Wei CHEN <83599072+Sunfacing@users.noreply.github.com>
2021-10-08 18:40:33 +0800

    test auto process

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "<h1 style='color:blue'>Hello There Again!</h1>"
 
 
 if __name__ == "__main__":
